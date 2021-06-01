ab5f48a87ab9cc810b21bfa994ab0bb179003043
Muhdsyarif <72724907+Muhdsyarif@users.noreply.github.com>
2021-06-02 07:30:03 +0800

    Add files via upload

---
+++ src/Advertisment.java
@@ -0,0 +1,19 @@
+
+public class Advertisment {
+	
+	private String event,giftCard,date;
+	
+	Advertisment(){
+		/*this.event = "CELEBRATE EVERY MOMENT";
+		this.giftCard = "Free 20% reward card";
+		this.date = "01.06.2021 - 30.06.2021";*/
+		
+		System.out.println("******CELEBRATE EVERY MOMENT AT HUNTER BOWLING******"+
+						 "\n***************Free 20% reward card*****************"+
+							"\nDate      ====>      1'st JUNE 2021 - 31 JUNE 2021"+
+							"\nContact us   ====>   017-72548864 or 03-126559"+
+							"\nEmail    ====>    HunterBowling@gmail.com");
+	}
+	 
+
+}
---
+++ src/Bowling.java
@@ -0,0 +1,15 @@
+
+public class Bowling {
+	
+	
+	void printBowling(String name, String location, String phoneNum, double open, double close, String holiday){
+		System.out.println("Sport Name     : " + name);
+		System.out.println("Sport Location : " + location);
+		System.out.println("Phone Number   : " + phoneNum);
+		System.out.println("Open Time      : " + open + " a.m");
+		System.out.println("Close Time     : " + close + " p.m");
+		System.out.println("Holiday        : " + holiday);
+	} 
+
+
+}
